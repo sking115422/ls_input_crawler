72d9ddd56b5abc9ec4e33d70e13223c08c7d99e7
sking115422 <spencerd.king@gmail.com>
2023-09-28 13:04:54 -0400

    added clean_img script and js_crawler

--- ss_crawler.js
+++ ss_crawler.js
@@ -1,6 +1,11 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs');
 const config = require('config');
+const cheerio = require('cheerio');
+
+// Reading in stop words
+f = fs.readFileSync("./stop_word_list/stop_word_list_174.txt", {encoding: 'utf8', flag: "r"})
+sw_list = f.split("\r\n")
 
 // Function generates a list of urls specified in a text file for processing
 function getUrlsFromTextFile (path) {
@@ -32,7 +37,9 @@ async function get_ss (ind, uri) {
     try {
 
         const browser = await puppeteer.launch({headless:true})
-        const page = await browser.newPage()
+        const page = await browser.newPage(); //open new tab
+        await (await browser.pages())[0].close(); //close first one, to overcome the bug in stealth library mentioned in
+        //https://github.com/berstend/puppeteer-extra/issues/88
 
         await page.setViewport({
             height: 1080,
@@ -46,10 +53,57 @@ async function get_ss (ind, uri) {
             // timeout: 0
         })
 
-        await sleep(5000)
+        await sleep(1000)
+
+        const t_hold = .35
+        let wp_eng = false
+
+        wp_lang = await page.evaluate(() => {
+
+            const lang = document.querySelector('html').lang
+            return lang
+
+        })
+
+        if (wp_lang == "en") {
+            wp_eng = true
+        } else {
+            allElements = await page.evaluate(() => {
+                const tmp_list = []
+                const allElements = document.querySelectorAll('*')
+                for (const element of allElements) {
+                    tmp_list.push(element.innerText)
+                }             
+                return tmp_list
+            })
+
+            en_count = 0
+            word_count = 0
+
+            for (let i = 0; i < allElements.length; i++) {
+                elemWords = allElements[i].split(" ")
+                bf = false
+                for (let j = 0; j < elemWords.length; j++) {
+                    word_count++
+                    for (k = 0; k < sw_list.length; k++){
+                        if (elemWords[j] == sw_list[k]) {
+                            en_count ++
+                        }
+                    } 
+                }
+            }
+
+            eng_w_per = en_count/word_count
+        }
+
+        if (eng_w_per < t_hold) {
+            throw {name: "nonEnglishWebpageException", message: "This webpage is not in english so it will be skipped!"};
+        }
+
+        await sleep(2000)
 
         await page.screenshot({
-            path: `/home/spenc/research/dtron2_ls_uploader/ss/ss_${ind}.png`,
+            path: `./ss/ss_${ind}.png`,
             clip: {
                 x:0,
                 y:0,
