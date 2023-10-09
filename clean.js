fac4506359e635664a3ac6999dd2fa30f4242477
sking115422 <spencerd.king@gmail.com>
2023-10-09 17:48:48 -0400

    created cleaning script

---
+++ clean.js
@@ -0,0 +1,27 @@
+// Importing Libraries
+const fs = require('fs');
+const config = require('config');
+
+// Reading Config
+const paths = config.get('paths')
+let uri_output_dir_ = paths.uri_output_dir
+let ckpt_dir_ = paths.ckpt_dir
+
+// Cleaing Output Dir
+let output_dir_list = fs.readdirSync(uri_output_dir_)
+for (i=0; i<output_dir_list.length; i++) {
+    let name = output_dir_list[i]
+    let parts = name.split("__")
+    let ending = parts[parts.length - 1]
+    if (ending == "exp.txt") {
+        fs.unlinkSync(uri_output_dir_ + name)
+    }
+}
+
+// Cleaning Checkpoint Dir
+if (fs.existsSync(ckpt_dir_ + "ret_list.json"))
+    fs.unlinkSync(ckpt_dir_ + "ret_list.json")
+if (fs.existsSync(ckpt_dir_ + "del_list.json"))
+    fs.unlinkSync(ckpt_dir_ + "del_list.json")
+if (fs.existsSync(ckpt_dir_ + "url_visited.txt"))
+    fs.unlinkSync(ckpt_dir_ + "url_visited.txt")
